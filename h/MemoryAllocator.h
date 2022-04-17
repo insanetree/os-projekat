99cd469c85c290fa82c6cd52098baebcba69bf91
insanetree <p0p0v1c.41ek54nd4r@gmail.com>
2022-04-18 00:37:27 +0200

    Memory Allocator init file

---
+++ h/MemoryAllocator.h
@@ -0,0 +1,6 @@
+#ifndef _MEMORYALLOCATOR_H
+#define _MEMORYALLOCATOR_H
+struct Memory_Block {
+
+};
+#endif //_MEMORYALLOCATOR_H
--- src/main.c
+++ src/main.c
@@ -8,10 +8,9 @@ void main(){
 	printstr("hello\n");
 	uint64 start = (uint64)HEAP_START_ADDR;
 	uint64 end = (uint64)HEAP_END_ADDR;
-    printunum(123);
-    printstr("\n");
-    printunum(start);
-    printstr("\n");
-    printunum(end);
-    printstr("\n");
+	printunum((end-start)/MEM_BLOCK_SIZE);
+	printstr("\n");
+	int* t = (int*)HEAP_START_ADDR;
+	*t = 10;
+	printunum(*t);
 }
