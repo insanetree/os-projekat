b1d52cfab89e9d350178c18365c2ded2eefc9bc4
insanetree <p0p0v1c.41ek54nd4r@gmail.com>
2022-04-18 01:49:07 +0200

    Inicijalizacija memorije

--- h/MemoryAllocator.h
+++ h/MemoryAllocator.h
@@ -1,6 +1,17 @@
 #ifndef _MEMORYALLOCATOR_H
 #define _MEMORYALLOCATOR_H
-struct Memory_Block {
 
+#include "../lib/hw.h"
+
+struct Memory_block {
+	size_t size;
+	struct Memory_block* next;
 };
+
+void memory_init();
+
+void* block_alloc(size_t size);
+
+void block_free(void* block, size_t size);
+
 #endif //_MEMORYALLOCATOR_H
--- src/MemoryAllocator.c
+++ src/MemoryAllocator.c
@@ -1,1 +1,8 @@
+#include "../h/MemoryAllocator.h"
+
+void memory_init() {
+	struct Memory_block* bgn = (struct Memory_block*)HEAP_START_ADDR;
+	bgn->next = 0;
+	bgn->size = ((uint64)HEAP_END_ADDR-(uint64)HEAP_START_ADDR+1)/MEM_BLOCK_SIZE;
+}
 
