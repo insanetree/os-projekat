ab2dc6324865eac1126a4da90000ee6aa6836d0c
insanetree <p0p0v1c.41ek54nd4r@gmail.com>
2022-06-10 02:56:06 +0200

    fookin done

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,9 @@
 #include "../h/syscall_c.h"
 #include "../h/sleeper.h"
 
-thread_t usr;
-
-void userMain(void* );
-
 int main() {
 	__init_system();
 
-	thread_create(&usr, &userMain, NULL);
-
-	while(usr->state != FINISHED)
-		thread_dispatch();
 
 	return 0;
 }
