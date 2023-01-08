88198735929efcb50fd383ed14c38ee690766b56
insanetree <p0p0v1c.41ek54nd4r@gmail.com>
2023-01-08 16:01:54 +0100

    makefile updated - include dir option

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "../lib/hw.h"
 
 //my header files
-#include "../h/kernellib.h"
-#include "../h/syscall_c.h"
-#include "../h/sleeper.h"
+#include "kernellib.h"
+#include "syscall_c.h"
+#include "sleeper.h"
 
 int main() {
 	__init_system();
